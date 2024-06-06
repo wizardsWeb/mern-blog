4cf194c649249b8ae71813048236fd56d23e3cba
Vinay Vora <122713139+wizardsWeb@users.noreply.github.com>
2024-06-06 21:59:11 +0530

    create and run the backend server

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -35,7 +35,7 @@ const Header = () => {
             </Button>
 
             <Link to='/sign-in'>
-                <Button gradientDuoTone='purpleToBlue'>
+                <Button gradientDuoTone='purpleToBlue' outline>
                     Sign In
                 </Button>
             </Link>
