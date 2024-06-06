a2493b34cc148e8a9638ea33ff7639fabaf3b926
Vinay Vora <122713139+wizardsWeb@users.noreply.github.com>
2024-06-06 22:34:52 +0530

    connect to the database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = express();
 const PORT = 3000;
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+    .then(() => {
+        console.log("Database successfully connected");
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
 
 app.get("/", (req, res) => {
     res.send("Hello World");
