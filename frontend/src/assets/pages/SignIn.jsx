dbcadeb08a11ba3cd4ca237101775257e6c078f5
Vinay Vora <122713139+wizardsWeb@users.noreply.github.com>
2024-06-15 02:28:08 +0530

    Complete signin page functionality

--- frontend/src/assets/pages/SignIn.jsx
+++ frontend/src/assets/pages/SignIn.jsx
@@ -1,10 +1,127 @@
-import React from 'react'
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 const SignIn = () => {
+  const [formData, setFormData] = useState({});
+
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const [loading, setLoading] = useState(false);
+
+  // Initializing the navigate 
+  const navigate  = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim()})
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // change the proxy in the vite.config.js
+
+    if(!formData.email || !formData.password) {
+      return setErrorMessage("All fields are required");
+    }
+
+    try {
+
+      // Setting the loading effect
+      setLoading(true);
+      setErrorMessage(null);
+
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: { 'content-type' : 'application/json' },
+        body: JSON.stringify(formData)
+      });
+      const data = await res.json();
+
+      if(data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+
+      // used react useNavigate hook
+      if(res.ok) {
+        navigate('/')
+      }
+
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  }
+
   return (
-    <div>
-      SignIn
-    </div>
+    <>
+      <div className="min-h-screen mt-20">
+        <div className="flex gap-5 p-3 max-w-3xl mx-auto flex-col  md:flex-row md:items-center">
+
+          {/* Left */}
+          <div className="flex-1">
+            <Link to="/" className='text-4xl sm:text-xl font-bold dark:text-white'>
+                <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+                    webwizards
+                </span>
+                    blog
+            </Link>
+            <p className='text-xm mt-5'>
+              This is demo project. You can sign in with your email and password
+              or with Google.
+            </p>
+          </div>
+
+          {/* Right */}
+          <div className='flex-1'>
+            <form className='flex flex-col gap-4' onSubmit={handleSubmit}>  
+              <div>
+                <Label value='Your Email' />
+                <TextInput
+                  type='email'
+                  placeholder='name@company.com'
+                  id='email'
+                  onChange={handleChange}
+                  />
+              </div>
+              <div>
+                <Label value='Your Password' />
+                <TextInput
+                  type='password'
+                  placeholder='***********'
+                  id='password'
+                  onChange={handleChange}
+                  />
+              </div>
+              <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>
+                {
+                  loading ? (
+                    <>
+                    <Spinner size='sm'/>
+                    <span className='pl-3'>Loading ...</span>
+                    </>
+                  ) : 'Sign In'
+                }
+              </Button>
+            </form>
+            <div className="text-sm mt-3">
+              <Link to='/sign-up' className='text-blue-500'>
+              <span className='text-black'>Dont Have an Account? </span>
+                Sign Up
+              </Link>
+            </div>
+            { 
+              errorMessage && (
+                <Alert className='mt-5' color='failure'>
+                  {errorMessage}
+                </Alert>
+              ) 
+            }
+          </div>
+        </div>
+      </div> 
+    </>
   )
 }
 
