40148da044412c64d4fa48a7d412b726ab4d1953
Vinay Vora <122713139+wizardsWeb@users.noreply.github.com>
2024-06-20 18:04:19 +0530

    add admin functionality to the user

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -18,6 +18,10 @@ const userSchema = new mongoose.Schema({
     profilePicture: {
         type: String,
         default: "https://static.vecteezy.com/system/resources/thumbnails/005/544/718/small_2x/profile-icon-design-free-vector.jpg",
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
     }
 },
 {
