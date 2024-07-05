a3a9d775743e81eccb337a036963927d8603e7cf
Vinay Vora <122713139+wizardsWeb@users.noreply.github.com>
2024-07-05 18:44:23 +0530

    Add delete functionality to the comment section

--- backend/controllers/comment.controller.js
+++ backend/controllers/comment.controller.js
@@ -63,4 +63,47 @@ const likeComment = async(req, res, next) => {
     }
 }
 
-module.exports = {createComment, getPostComments, likeComment};
+const editComment = async(req, res, next) => {
+    try {
+      const comment = await Comment.findById(req.params.commentId);
+
+      if(!comment) {
+        return next(errorHandler(404, 'Comment Not Found'));
+      }
+
+      if(comment.userId !== req.user.id && !req.user.isAdmin) {
+        return (next(errorHandler(403, 'You are not allowed to edit this comment')))
+      }
+
+      const editedComment = await Comment.findByIdAndUpdate(req.params.commentId, {
+        content: req.body.content
+      }, { new: true});
+
+      res.status(200).json(editedComment);
+
+    } catch (error) {
+      next(error);
+    }
+}
+
+const deleteComment = async(req, res, next) => {
+  try {
+    const comment = await Comment.findById(req.params.commentId);
+
+    if(!comment) {
+      return next(errorHandler(404, 'Comment Not found'));
+    }
+
+    if(comment.userId !== req.user.id && !req.user.isAdmin) {
+      return (next(errorHandler(403, 'You are not allowed to delete this comment')));
+    }
+
+    await Comment.findByIdAndDelete(req.params.commentId)
+    res.status(200).json('Comment has been deleted');
+
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = {createComment, getPostComments, likeComment, editComment, deleteComment};
--- backend/routes/comment.route.js
+++ backend/routes/comment.route.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const commentRouter = express.Router();
 const verifyToken = require('../utils/verifyUser');
-const { createComment, getPostComments, likeComment } = require('../controllers/comment.controller');
+const { createComment, getPostComments, likeComment, editComment, deleteComment } = require('../controllers/comment.controller');
 
 commentRouter.post('/create', verifyToken,  createComment)
 commentRouter.get('/getPostComments/:postId', getPostComments);
 commentRouter.put('/likeComment/:commentId', verifyToken, likeComment);
+commentRouter.put('/editComment/:commentId', verifyToken, editComment);
+commentRouter.delete('/deleteComment/:commentId', verifyToken, deleteComment);
 
 module.exports = commentRouter;
